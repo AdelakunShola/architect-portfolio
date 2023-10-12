176e40e07459b9be09c18675055b465a18713506
Adelakun Shola <officialadelakun@gmail.com>
2023-10-12 07:29:29 -1100

    team

--- app/Http/Controllers/Backend/TeamController.php
+++ app/Http/Controllers/Backend/TeamController.php
@@ -5,7 +5,8 @@ namespace App\Http\Controllers\Backend;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Team;
-
+use Carbon\Carbon;
+use Intervention\Image\Facades\Image;
 class TeamController extends Controller
 {
 
@@ -29,8 +30,27 @@ public function StoreTeam(Request $request){
 
     $image = $request->file('image');
     $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtention();
-    Image::make
+    Image::make($image)->resize(562,562)->save('upload/team/'.$name_gen);
+    $save_url = 'upload/team/'.$name_gen;
 
-}// end method
+    Team::insert([
+
+        'name' => $request->name,
+        'position' => $request->position,
+        'instagram' => $request->instagram,
+        'twitter' => $request->twitter,
+        'youtube' => $request->youtube,
+        'image' => $save_url,
+        'created_at' => Carbon::now(),
+    ]);
+
+    $notification = array(
+        'message' => 'Team Data Inserted Successfully',
+        'alert-type' => 'success'
+    );
+
+    return redirect()->route('all.team')->with($notification);
+
+} // end method
 
 }
