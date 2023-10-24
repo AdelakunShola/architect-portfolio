0523d75036f54eeaa02fdd7232998805e14fc10f
Adelakun Shola <officialadelakun@gmail.com>
2023-10-24 07:47:30 -1100

    .

--- app/Http/Controllers/Backend/ProjectTypeController.php
+++ app/Http/Controllers/Backend/ProjectTypeController.php
@@ -4,8 +4,13 @@ namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\ProjectType;
 
 class ProjectTypeController extends Controller
 {
-    public function ProjectType(){} //end method
+    public function ProjectType(){
+
+        $allData = ProjectType::orderBy('id','desc')->get();
+        return view('backend.allproject.projecttype',compact('allData'));
+    } //end method
 }
