8e512c9d639489712dd63592a01f6464c1d15843
Adelakun Shola <officialadelakun@gmail.com>
2023-11-11 11:58:11 -1100

    .

--- resources/views/frontend/project/allproject.blade.php
+++ resources/views/frontend/project/allproject.blade.php
@@ -59,7 +59,29 @@
                        <!-- PAGINATION END -->
                    </div>
                    <!-- GALLERY CONTENT START -->
-
+                    <div class="portfolio-wrap mfp-gallery work-grid clearfix">
+                        <div class="container-fluid">
+                              <div class="row">
+                                   <!-- COLUMNS 1 -->
+                                   @foreach($project as $item)
+                                   <div class="masonry-item cat-1 col-lg-3 col-md-6 col-sm-6 m-b30">
+                                   
+                                       <div class="wt-img-effect ">
+                                           <img src="{{ asset('/'.$item->image) }}" alt="" style="width: 750px; height:400px;" >
+                                           <div class="overlay-bx-2 ">
+                                               <div class="line-amiation">
+                                                   <div class="text-white  font-weight-300 p-a40">
+                                                       <h2><a href="javascript:void(0);" class="text-white font-20 letter-spacing-4 text-uppercase">Triangle Concrete House on lake</a></h2>
+                                                       <p>Morbi mattis ex non urna condimentum,
+                                                             eget eleifend diam molestie. Curabitur 
+                                                             lorem enim, maximus non null.
+                                                       </p>
+                                                       <a href="javscript:;" class="v-button letter-spacing-4 font-12 text-uppercase p-l20">Read More</a>
+                                                   </div>
+                                               </div>
+                                          </div>
+                                       </div>
+                                        
                                    </div>
                                    @endforeach
                                                                                                                                 
